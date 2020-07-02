9604a8d755f19dc90143d72aab9a6a2714babbf5
elkinjimenez <elkin.jiga@hotmail.com>
2020-07-02 12:55:35 -0500

    Ajustes header y footer

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
+import { BannerComponent } from './banner/banner.component';
 
 
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent, HomeComponent],
+  declarations: [HeaderComponent, FooterComponent, HomeComponent, BannerComponent],
   imports: [
-    CommonModule
+    CommonModule,
   ]
 })
 export class SharedModule { }
