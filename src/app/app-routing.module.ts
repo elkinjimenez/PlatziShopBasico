4e309a51ec1944fb3b5d903aae1d83d038235244
elkinjimenez <elkin.jiga@hotmail.com>
2020-07-02 12:37:35 -0500

    Estructura de componentes compartidos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 import { ProductsComponent } from './modules/products/products.component';
 import { ProductComponent } from './modules/product/product.component';
 import { ContactsComponent } from './modules/contacts/contacts.component';
+import { HomeComponent } from './shared/home/home.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { SharedModule } from './shared/shared.module';
+import { ModulesModule } from './modules/modules.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    ModulesModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
