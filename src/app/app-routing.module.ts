05d1ecd9f9f5e8234c002fe897256014892d8d2f
elkinjimenez <elkin.jiga@hotmail.com>
2020-07-02 12:33:06 -0500

    Estructura del router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './modules/products/products.component';
+import { ProductComponent } from './modules/product/product.component';
+import { ContactsComponent } from './modules/contacts/contacts.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'product', component: ProductComponent },
+  { path: 'contacts', component: ContactsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/modules/modules.module.ts
+++ src/app/modules/modules.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductsComponent } from './products/products.component';
 import { ProductComponent } from './product/product.component';
+import { ContactsComponent } from './contacts/contacts.component';
 
 
 
 @NgModule({
-  declarations: [ProductsComponent, ProductComponent],
+  declarations: [ProductsComponent, ProductComponent, ContactsComponent],
   imports: [
     CommonModule
   ]
